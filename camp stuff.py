4333de369f79c8b62c96e2e0bb791ab75621c630
1085535 <vihaan.naveen@gmail.com>
2026-07-01 09:48:53 -0700

    update campstuff

--- camp stuff.py
+++ camp stuff.py
@@ -1,7 +1,14 @@
 age = 12
-
+name = "vihaan"
+fav_color="blue"
+fav_food="fried rice"
+lucky_number=9
 hobby = "tennis"
 hrsperwk = "5"
 print(f"My favorite hobby is {hobby} and i spend {hrsperwk} hours per week")
 print("hello")
 print("work please")
+
+print ("github is failing me")
+
+print("I am", name, "my favorite color is", fav_color,"and my favorite food is", fav_food, "hope i'll be lucky because my lucky number is", lucky_number,)
