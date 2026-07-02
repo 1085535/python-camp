1b03bc0ca9d679afa294cfdfb6d9d74b576fcd16
1085535 <vihaan.naveen@gmail.com>
2026-07-02 13:48:28 -0700

    co2 calc

--- Day 3 boom.py
+++ Day 3 boom.py
@@ -1,10 +1,46 @@
-a=9*3
-b=18/6
-c=4+7
-d=20-12
+#breathing, using devices, electricity, water, food, transportation. 
+bus=0.04
+computers=0.06
+electriccar = 0.14
+gascar=0.4
+ledlight=0.02
+lightrail=1.3
+shower=1.2
+electricalstove=0.3
+ac1=2.5
+natgas=0.45
 
-list = [a,b,c,d]
+print("welcome...to my information steal-i mean my calculator")
+transport= input("How did you come here today?")
+miles = input("how many miles did you travel")
+food =input("How many meals cooked per day")
+stove=input("What kind of stove")
+lights1=input("How many hours are your lights on")
+light2=input("How many lights do you have")
+showers=input("How many showers per day")
+ac=input("How many hours did you have AC on")
+computer=input("How many hours on computer at home")
 
-print (list)
 
-print(not True)
+if transport=="bus":
+    transport=bus
+elif transport=="electric car":
+    transport=electriccar
+elif transport==gascar:
+    transport=gascar
+
+finaltransport=float(miles)*float(transport)
+
+if stove=="natural gas":
+    stove=natgas
+elif stove=="electrical stove":
+    stove=electricalstove
+
+finalstove=float(stove)*float(food)
+finallights=float(light2)*float(lights1)*ledlight
+finalac=float(ac)*ac1
+finalshower=float(showers)*shower
+finalcomputer=float(computer)*computers
+
+finalemissions=(finalstove)+(finalshower)+(finalcomputer)+(finallights)+(finaltransport)+(finalac)
+print("Your final CO2 emissions are", finalemissions, "kilos")
