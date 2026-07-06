ec478020d85210ef072fd9712ea4a020a1f080f7
1085535 <vihaan.naveen@gmail.com>
2026-07-06 09:41:42 -0700

    nothing

--- #day 4.py
+++ #day 4.py
@@ -1,6 +1,8 @@
 #number even or odd
 number = input("tell me a number")
-if number%2 == 1:
+if number == str():
+    ("this isnt a number =(")
+elif number%2 == 1:
     print("this number is odd")
 else:
     print("this number is odd")
