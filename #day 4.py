f2025c0f3c10c25e51c381abac1de4497db7cb3e
1085535 <vihaan.naveen@gmail.com>
2026-07-06 14:28:24 -0700

    tamagotchi

--- #day 4.py
+++ #day 4.py
@@ -1,18 +1,9 @@
-#number even or odd
-level=12
-weapon=False
-health =70
-has_armor=-False
-magic_ring=True
-if magic_ring==True:
-    level>=10
-    health>=50
-    weapon=True
-if level>=10:
-    if weapon or has_armor ==True:
-        if health >= 50:
-            print ("You're ready for battle")
-        else: print("you're health is too low")
-    else:
-        print("you need a weapon to fight")
-         
+
+for row in range(1,5):
+   for col in range (1,row+1):
+      print(col,end=" ")
+   print()
+    
+      
+    
+
---
+++ #tamagotchi proj.py
@@ -0,0 +1,55 @@
+#tamagotchi proj
+while True:
+    print(f"----------------------------------------\n kflWELCOME TO THIS INTERACTIVE SIMULATION\n----------------------------------------")
+    pet_name=input("What is your pet name")
+    pet_type=input("is your pet a dragon, cat, or dog")
+    if pet_type!="dragon" and pet_type!="cat" and pet_type!="dog":
+        print("invalid pet type")
+
+    hunger=100
+    happiness=100
+    health=3
+    sleep=10
+    water=5
+    cleanliness=10
+
+    print("\nYour pet is hatched and is very happy to see you! He is great at shooting free throws somehow and has mood swings ")
+
+    print("\n your pet's hunger is at",hunger,"your pet's happiness is at",happiness,"your pet has",health,"lives. your pet's sleep is at",sleep,"your pet's water is at",water,"your pet's cleanliness is at",cleanliness,"If your stats hit zero, you lose")
+
+    move= input("\nYou can feed your pet, play with your pet, take your pet to the doctor, put your pet to sleep, give your pet water, or clean your pet. You can also check on your pet's stats. Type 'exit' to exit the simulation. Respond with 'feed', 'play', 'doctor', 'sleep', 'water', 'clean', or 'stats' to make a move: ")
+    if move=="exit":
+        print("\nThanks for playing!")
+        break
+    if move!="feed" and move!="play" and move!="doctor" and move!="sleep" and move!="water" and move!="clean" and move!="stats":
+        print("\n sorry, that's not a valid move")
+    
+    if move=="feed":
+        hunger+=10
+    if move=="play":
+        happiness+=10
+    if move=="doctor":
+        health+=2
+    if move=="sleep":
+        sleep+=3
+    if move=="water":
+        water+=3
+    if move=="clean":
+        cleanliness+=5
+
+    if move!="clean":
+        cleanliness-=0.25
+    if move!="water":
+        water-=1
+    if move!="sleep":
+        sleep-=2
+    if move!="doctor":
+        health-=0.5
+    if move!="play":
+        happiness-=30
+    if move!="feed":
+        hunger-=20
+
+    if hunger<=0 or happiness<=0 or health<=0 or sleep<=0 or water<=0 or cleanliness<=0:
+        print("\n Your pet has died. Game over.")
+        break
