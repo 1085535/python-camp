c640fc9af151c14f26e5390ebd789d38f07c4adc
1085535 <vihaan.naveen@gmail.com>
2026-07-06 10:25:06 -0700

    ready for battle

--- #day 4.py
+++ #day 4.py
@@ -1,8 +1,18 @@
 #number even or odd
-number = input("tell me a number")
-if number == str():
-    ("this isnt a number =(")
-elif number%2 == 1:
-    print("this number is odd")
-else:
-    print("this number is odd")
+level=12
+weapon=False
+health =70
+has_armor=-False
+magic_ring=True
+if magic_ring==True:
+    level>=10
+    health>=50
+    weapon=True
+if level>=10:
+    if weapon or has_armor ==True:
+        if health >= 50:
+            print ("You're ready for battle")
+        else: print("you're health is too low")
+    else:
+        print("you need a weapon to fight")
+         
