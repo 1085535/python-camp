e313bc052d4269a7f02b14faa3cefce48902873a
1085535 <vihaan.naveen@gmail.com>
2026-07-08 12:49:50 -0700

    d

--- #habit tracker.py
+++ #habit tracker.py
@@ -4,5 +4,10 @@ import tkinter as tk
 
 window = tk.Tk()
 window.title("Habit Tracker")
-tk.Label(window, text="Habit Tracker")
+title_label =tk.Label(window, text="Habit Tracker")
+first_habit=tk.Label(window, text="Drink water-streak three days")
+habit_button=tk.Button(window, text="Mark as done")
+title_label.pack()
+first_habit.pack()
+habit_button.pack()
 window.mainloop()
