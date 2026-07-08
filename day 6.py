5a3d43a7065435bf5014811db0f8fe35cf6480d9
1085535 <vihaan.naveen@gmail.com>
2026-07-08 11:23:54 -0700

    list dictionnary wooaaah

---
+++ day 6.py
@@ -0,0 +1,4 @@
+person={"Name":"vihaan","city":"seattle","Subjects":["Math","Science","English","History"]}
+print("Student's name is",person["Name"])
+print("Student's city is",person["city"])
+print("Student's subjects are",person["Subjects"])
