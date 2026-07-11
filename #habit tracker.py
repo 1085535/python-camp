c5396cbed61f56e0f8f4a0d464cefbffec6cc03a
1085535 <vihaan.naveen@gmail.com>
2026-07-11 12:51:23 -0700

    added

--- #habit tracker.py
+++ #habit tracker.py
@@ -1,17 +1,31 @@
 #habit tracker
 
 import tkinter as tk
-
+def addfunc():
+    new_habit_name = entry_widget.get()
+    habits.append(new_habit_name)
+    print(habits)
+    entry_widget.delete(0,tk.END)
 window = tk.Tk()
 window.title("Habit Tracker")
 title_label =tk.Label(window, text="Habit Tracker")
 first_habit=tk.Label(window, text="Drink water-streak three days")
 def mark_done():
     print("clicked!")
-habit_button=tk.Button(window, text="Mark as done",command=mark_done)
+mark_as_done=tk.Button(window, text="Mark as done",command=mark_done)
+habit_button=tk.Button(window, text="Add habit", command=addfunc)
+habit_button.pack()
 title_label.pack()
 first_habit.pack()
-habit_button.pack()
-window.mainloop()
+mark_as_done.pack()
 
-entry_widget=tk.Entry(parent_window)
+
+entry_widget=tk.Entry(window)
+entry_widget.pack()
+
+habits=[]
+
+
+    
+
+window.mainloop()
